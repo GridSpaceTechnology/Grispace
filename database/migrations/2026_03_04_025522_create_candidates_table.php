05cfab7da59573dc48db87153d4a955b6194c446
B_Nwaochei <nwaocheibenjamin@gmail.com>
2026-04-15 18:12:27 +0100

    Fix migration error

--- database/migrations/2026_03_04_025522_create_candidates_table.php
+++ database/migrations/2026_03_04_025522_create_candidates_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('candidates', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('current_role', 255)->nullable()->index();
             $table->string('desired_role', 255)->nullable()->index();
             $table->unsignedSmallInteger('years_of_experience')->nullable()->index();
